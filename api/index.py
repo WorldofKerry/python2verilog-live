ec84ae7284be947e8691e61eae8fa770c3e2981d
Kerry Wang <kerrywang369@gmail.com>
2023-10-12 10:07:44 -0600

    looks good

--- api/index.py
+++ api/index.py
@@ -1,11 +1,44 @@
-from flask import Flask
+import tempfile
+import traceback
+from importlib import util
+
+from flask import Flask, jsonify, render_template, request
+
+from python2verilog.api.namespace import get_namespace, namespace_to_verilog
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+def tempfile_wrapper(raw: str):
+    raw = "from python2verilog import verilogify\n" + raw
+
+    # Create a temporary source code file
+    with tempfile.NamedTemporaryFile(suffix=".py") as tmp:
+        tmp.write(raw.encode())
+        tmp.flush()
+
+        try:
+            spec = util.spec_from_file_location("tmp", tmp.name)
+            module = util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+
+            ns = get_namespace(tmp.name)
+            result, _ = namespace_to_verilog(ns)
+        except Exception:
+            result = traceback.format_exc(limit=1)
+        finally:
+            return result
+
+
+@app.route("/api/update", methods=["POST"])
+def update_text():
+    text = request.form["text"]
+    text = tempfile_wrapper(text)
+    updated_text = text
+    return jsonify(updated_text=updated_text)
